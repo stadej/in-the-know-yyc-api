baf87c0fee7cffba7cd74fbf5655d9cf6f0f9cc9
Dmytro Masliuk <gitspace.md@gmail.com>
2024-09-09 17:55:47 -0600

    Initializing back-end api project

---
+++ src/main/java/com/intheknowyyc/in_the_know_yyc_api/InTheKnowYycApiApplication.java
@@ -0,0 +1,13 @@
+package com.intheknowyyc.in_the_know_yyc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InTheKnowYycApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InTheKnowYycApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intheknowyyc/in_the_know_yyc_api/InTheKnowYycApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intheknowyyc.in_the_know_yyc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InTheKnowYycApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
