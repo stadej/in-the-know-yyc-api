412a568d88c0e61661e5ef8256e417acf276a5a8
Unforgiven <mail.unforgiven@gmail.com>
2024-10-09 20:42:13 -0600

    Added API for working with AWS S3 (test).

--- src/main/java/com/intheknowyyc/api/config/StorageConfig.java
+++ src/main/java/com/intheknowyyc/api/config/StorageConfig.java
@@ -1,8 +1,5 @@
 package com.intheknowyyc.api.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,22 +9,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StorageConfig {
 
-    //@Value("${cloud.aws.credentials.access-key}")
-    //private String accessKey;
-
-    //@Value("${cloud.aws.credentials.secret-key}")
-    //private String secretKey;
-
-    @Value("${cloud.aws.region}")
-    private String region;
-
     @Bean
     public AmazonS3 S3Client() {
-        //AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        //return AmazonS3ClientBuilder.standard()
-                //.withCredentials(new AWSStaticCredentialsProvider(credentials))
-                //.withRegion(region)
-                //.build();
         return AmazonS3ClientBuilder.defaultClient();
     }
 }
