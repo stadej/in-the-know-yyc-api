430bad5d7254650c81c9eb151b80661923a06c81
Dmytro Masliuk <gitspace.md@gmail.com>
2024-11-04 19:15:25 -0700

    feat: Error handling for subscription service
    Deleted unneeded code from controller

--- src/main/java/com/intheknowyyc/api/controllers/SubscriptionController.java
+++ src/main/java/com/intheknowyyc/api/controllers/SubscriptionController.java
@@ -1,8 +1,6 @@
 package com.intheknowyyc.api.controllers;
 
 import com.intheknowyyc.api.controllers.requests.SubscriptionRequest;
-import com.intheknowyyc.api.data.exceptions.BadRequestException;
-import com.intheknowyyc.api.data.exceptions.ResourceNotFoundException;
 import com.intheknowyyc.api.services.SubscriptionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,10 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestClientException;
 
 /**
  * REST controller for managing Mailchimp subscription.
@@ -38,17 +33,8 @@ public class SubscriptionController {
             @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
     })
     @PostMapping("/subscribe")
-    public ResponseEntity<String> subscribe(@Valid @RequestBody SubscriptionRequest request) {
-        String response = subscriptionService.subscribe(request);
-        try {
-            return ResponseEntity.ok().body(response);
-        } catch (BadRequestException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        } catch (ResourceNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (RestClientException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
+    public String subscribe(@Valid @RequestBody SubscriptionRequest request) {
+        return subscriptionService.subscribe(request);
     }
 
 }
